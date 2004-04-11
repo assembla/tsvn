8bcd78eac7c6d3fbc7ffd0a6e1740589615c58e3
dimentiy <dimentiy@154be670-7969-47af-9ab0-c6fb378d2295>
2004-04-11 11:24:57 +0000

    For experiments with WiX.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code@1018 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ branches/dimentiy_wix/src/SVN/SVNStatus.cpp
@@ -0,0 +1,620 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2004 - Tim Kemp and Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include "stdafx.h"
+#include "resource.h"
+#include "..\\TortoiseShell\\resource.h"
+
+#include "SVNStatus.h"
+#include "UnicodeUtils.h"
+#ifdef _MFC_VER
+#	include "MessageBox.h"
+#	include "registry.h"
+#endif
+
+SVNStatus::SVNStatus(void)
+{
+	apr_initialize();
+	m_pool = svn_pool_create (NULL);				// create the memory pool
+	memset (&ctx, 0, sizeof (ctx));
+#ifdef _MFC_VER
+	svn_config_ensure(NULL, m_pool);
+	hWnd = NULL;
+	m_app = NULL;
+
+	// set up authentication
+	svn_auth_provider_object_t *provider;
+
+	/* The whole list of registered providers */
+	apr_array_header_t *providers = apr_array_make (m_pool, 10, sizeof (svn_auth_provider_object_t *));
+
+	/* The main disk-caching auth providers, for both
+	'username/password' creds and 'username' creds.  */
+	svn_client_get_simple_provider (&provider, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_username_provider (&provider, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+
+	/* The server-cert, client-cert, and client-cert-password providers. */
+	svn_client_get_ssl_server_trust_file_provider (&provider, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_ssl_client_cert_file_provider (&provider, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_ssl_client_cert_pw_file_provider (&provider, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+
+	/* Two prompting providers, one for username/password, one for
+	just username. */
+	svn_client_get_simple_prompt_provider (&provider, (svn_auth_simple_prompt_func_t)simpleprompt, this, 2, /* retry limit */ m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_username_prompt_provider (&provider, (svn_auth_username_prompt_func_t)userprompt, this, 2, /* retry limit */ m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+
+	/* Three prompting providers for server-certs, client-certs,
+	and client-cert-passphrases.  */
+	svn_client_get_ssl_server_trust_prompt_provider (&provider, sslserverprompt, this, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_ssl_client_cert_prompt_provider (&provider, sslclientprompt, this, 2, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+	svn_client_get_ssl_client_cert_pw_prompt_provider (&provider, sslpwprompt, this, 2, m_pool);
+	APR_ARRAY_PUSH (providers, svn_auth_provider_object_t *) = provider;
+
+	/* Build an authentication baton to give to libsvn_client. */
+	svn_auth_open (&m_auth_baton, providers, m_pool);
+	ctx.auth_baton = m_auth_baton;
+
+	// set up the configuration
+	m_err = svn_config_get_config (&(ctx.config), NULL, m_pool);
+
+	if (m_err)
+	{
+		::MessageBox(NULL, this->GetLastErrorMsg(), _T("TortoiseSVN"), MB_ICONERROR);
+		svn_pool_destroy (m_pool);					// free the allocated memory
+		apr_terminate();
+		exit(-1);
+	} // if (m_err) 
+
+	//set up the SVN_SSH param
+	CString tsvn_ssh = CRegString(_T("Software\\TortoiseSVN\\SSH"));
+	tsvn_ssh.Replace('\\', '/');
+	if (!tsvn_ssh.IsEmpty())
+	{
+		svn_config_t * cfg = (svn_config_t *)apr_hash_get ((apr_hash_t *)ctx.config, SVN_CONFIG_CATEGORY_CONFIG,
+			APR_HASH_KEY_STRING);
+		svn_config_set(cfg, SVN_CONFIG_SECTION_TUNNELS, "ssh", CUnicodeUtils::GetUTF8(tsvn_ssh));
+	}
+#endif
+}
+
+SVNStatus::~SVNStatus(void)
+{
+	svn_pool_destroy (m_pool);					// free the allocated memory
+	apr_terminate();
+}
+
+void SVNStatus::ClearPool()
+{
+	svn_pool_clear(m_pool);
+}
+
+#ifdef _MFC_VER
+void SVNStatus::SaveAuthentication(BOOL save)
+{
+	if (save)
+	{
+		svn_auth_set_parameter(ctx.auth_baton, SVN_AUTH_PARAM_NO_AUTH_CACHE, NULL);
+	}
+	else
+	{
+		svn_auth_set_parameter(ctx.auth_baton, SVN_AUTH_PARAM_NO_AUTH_CACHE, (void *) "");
+	}
+}
+
+CString SVNStatus::GetLastErrorMsg()
+{
+	CString msg;
+	if (m_err != NULL)
+	{
+		svn_error_t * ErrPtr = m_err;
+		msg = CUnicodeUtils::GetUnicode(ErrPtr->message);
+		while (ErrPtr->child)
+		{
+			ErrPtr = ErrPtr->child;
+			msg += _T("\n");
+			msg += CUnicodeUtils::GetUnicode(ErrPtr->message);
+		}
+		return msg;
+	}
+	return _T("");
+}
+#else
+stdstring SVNStatus::GetLastErrorMsg()
+{
+	stdstring msg;
+	if (m_err != NULL)
+	{
+		svn_error_t * ErrPtr = m_err;
+#ifdef UNICODE
+		msg = CUnicodeUtils::StdGetUnicode(ErrPtr->message);
+#else
+		msg = ErrPtr->message;
+#endif
+		while (ErrPtr->child)
+		{
+			ErrPtr = ErrPtr->child;
+			msg += _T("\n");
+#ifdef UNICODE
+			msg += CUnicodeUtils::StdGetUnicode(ErrPtr->message);
+#else
+			msg += ErrPtr->message;
+#endif
+		} // while (m_err->child)
+		return msg;
+	} // if (m_err != NULL)
+	return msg;
+}
+#endif
+
+//static method
+svn_wc_status_kind SVNStatus::GetAllStatus(const TCHAR * path, BOOL recursive)
+{
+	//svn_auth_baton_t *			auth_baton;
+	svn_client_ctx_t 			ctx;
+	//apr_hash_t *				statushash;
+	svn_wc_status_kind			statuskind;
+	apr_pool_t *				pool;
+	svn_error_t *				err;
+	const char *				internalpath;
+	BOOL						isDir;
+
+	TCHAR						pathbuf[MAX_PATH];
+	_tcscpy(pathbuf, path);
+	isDir = PathIsDirectory(path);
+	if (!isDir)
+	{
+		TCHAR * ptr = _tcsrchr(pathbuf, '\\');
+		if (ptr == 0)
+			ptr = _tcsrchr(pathbuf, '/');
+		if (ptr == 0)
+			return svn_wc_status_unversioned;
+		*ptr = 0;
+	} // if (!isDir)
+	_tcscat(pathbuf, _T("\\"));
+	_tcscat(pathbuf, _T(SVN_WC_ADM_DIR_NAME));
+	if (!PathFileExists(pathbuf))
+		return svn_wc_status_unversioned;
+	if ((isDir)&&(!recursive))
+		return svn_wc_status_normal;
+
+	apr_initialize();
+	pool = svn_pool_create (NULL);				// create the memory pool
+	memset (&ctx, 0, sizeof (ctx));
+
+	//we need to convert the path to subversion internal format
+	//the internal format uses '/' instead of the windows '\'
+	internalpath = svn_path_internal_style (CUnicodeUtils::StdGetUTF8(path).c_str(), pool);
+
+	svn_revnum_t youngest = SVN_INVALID_REVNUM;
+	svn_opt_revision_t rev;
+	rev.kind = svn_opt_revision_unspecified;
+	statuskind = svn_wc_status_none;
+	err = svn_client_status (&youngest,
+							internalpath,
+							&rev,
+							getallstatus,
+							&statuskind,
+							recursive,	//descend
+							TRUE,		//getall
+							FALSE,		//update
+							TRUE,		//noignore
+							&ctx,
+							pool);
+
+	// Error present
+	if (err != NULL)
+	{
+		svn_pool_destroy (pool);				//free allocated memory
+		apr_terminate();
+		return svn_wc_status_unversioned;	
+	}
+
+	svn_pool_destroy (pool);				//free allocated memory
+	apr_terminate();
+	return statuskind;
+}
+
+//static method
+svn_wc_status_kind SVNStatus::GetAllStatusRecursive(const TCHAR * path)
+{
+	return GetAllStatus(path, TRUE);
+}
+
+//static method
+svn_wc_status_kind SVNStatus::GetMoreImportant(svn_wc_status_kind status1, svn_wc_status_kind status2)
+{
+	if (GetStatusRanking(status1) >= GetStatusRanking(status2))
+		return status1;
+	return status2;
+}
+//static private method
+int SVNStatus::GetStatusRanking(svn_wc_status_kind status)
+{
+	switch (status)
+	{
+		case svn_wc_status_none:
+			return 0;
+		case svn_wc_status_unversioned:
+			return 1;
+		case svn_wc_status_ignored:
+			return 2;
+		case svn_wc_status_incomplete:
+			return 4;
+		case svn_wc_status_normal:
+		case svn_wc_status_external:
+			return 5;
+		case svn_wc_status_added:
+			return 6;
+		case svn_wc_status_missing:
+			return 7;
+		case svn_wc_status_deleted:
+			return 8;
+		case svn_wc_status_replaced:
+			return 9;
+		case svn_wc_status_modified:
+			return 10;
+		case svn_wc_status_merged:
+			return 11;
+		case svn_wc_status_conflicted:
+			return 12;
+		case svn_wc_status_obstructed:
+			return 13;
+	} // switch (status)
+	return 0;
+}
+
+svn_revnum_t SVNStatus::GetStatus(const TCHAR * path, bool update /* = false */, bool noignore /* = false */)
+{
+	apr_hash_t *				statushash;
+	apr_array_header_t *		statusarray;
+	const sort_item*			item;
+	const char *				internalpath;
+
+	//we need to convert the path to subversion internal format
+	//the internal format uses '/' instead of the windows '\'
+	internalpath = svn_path_internal_style (CUnicodeUtils::StdGetUTF8(path).c_str(), m_pool);
+
+	statushash = apr_hash_make(m_pool);
+	svn_revnum_t youngest = SVN_INVALID_REVNUM;
+	svn_opt_revision_t rev;
+	rev.kind = svn_opt_revision_unspecified;
+	struct hashbaton_t hashbaton;
+	hashbaton.hash = statushash;
+	hashbaton.pool = m_pool;
+	m_err = svn_client_status (&youngest,
+							internalpath,
+							&rev,
+							getstatushash,
+							&hashbaton,
+							FALSE,		//descend
+							TRUE,		//getall
+							update,		//update
+							noignore,		//noignore
+							&ctx,
+							m_pool);
+
+
+	// Error present if function is not under version control
+	if ((m_err != NULL) || (apr_hash_count(statushash) == 0))
+	{
+		status = NULL;
+		return -2;	
+	}
+
+	// Convert the unordered hash to an ordered, sorted array
+	statusarray = sort_hash (statushash,
+							  sort_compare_items_as_paths,
+							  m_pool);
+
+	//only the first entry is needed (no recurse)
+	item = &APR_ARRAY_IDX (statusarray, 0, const sort_item);
+	
+	status = (svn_wc_status_t *) item->value;
+	
+	return youngest;
+}
+svn_wc_status_t * SVNStatus::GetFirstFileStatus(const TCHAR * path, const TCHAR ** retPath, bool update)
+{
+	const sort_item*			item;
+	const char *				internalpath;
+
+	//we need to convert the path to subversion internal format
+	//the internal format uses '/' instead of the windows '\'
+	internalpath = svn_path_internal_style (CUnicodeUtils::StdGetUTF8(path).c_str(), m_pool);
+
+
+	m_statushash = apr_hash_make(m_pool);
+	svn_revnum_t youngest = SVN_INVALID_REVNUM;
+	svn_opt_revision_t rev;
+	rev.kind = svn_opt_revision_unspecified;
+	struct hashbaton_t hashbaton;
+	hashbaton.hash = m_statushash;
+	hashbaton.pool = m_pool;
+	m_err = svn_client_status (&youngest,
+							internalpath,
+							&rev,
+							getstatushash,
+							&hashbaton,
+							TRUE,		//descend
+							TRUE,		//getall
+							update,		//update
+							TRUE,		//noignore
+							&ctx,
+							m_pool);
+
+
+	// Error present if function is not under version control
+	if ((m_err != NULL) || (apr_hash_count(m_statushash) == 0))
+	{
+		return NULL;	
+	}
+
+	// Convert the unordered hash to an ordered, sorted array
+	m_statusarray = sort_hash (m_statushash,
+								sort_compare_items_as_paths,
+								m_pool);
+
+	//only the first entry is needed (no recurse)
+	m_statushashindex = 0;
+	item = &APR_ARRAY_IDX (m_statusarray, m_statushashindex, const sort_item);
+#ifdef UNICODE
+	int len = MultiByteToWideChar(CP_UTF8, 0, (const char *)item->key, -1, 0, 0);
+	*retPath = (const TCHAR *)apr_palloc(m_pool, len * sizeof(TCHAR));
+	MultiByteToWideChar(CP_UTF8, 0, (const char *)item->key, -1, (LPWSTR)*retPath, len);
+#else
+	if (svn_utf_cstring_from_utf8 (retPath, (const char *)item->key, m_pool) != NULL)
+		return NULL;
+#endif
+	return (svn_wc_status_t *) item->value;
+}
+
+svn_wc_status_t * SVNStatus::GetNextFileStatus(const TCHAR ** retPath)
+{
+	const sort_item*			item;
+
+	if ((m_statushashindex+1) == apr_hash_count(m_statushash))
+		return NULL;
+	m_statushashindex++;
+
+	item = &APR_ARRAY_IDX (m_statusarray, m_statushashindex, const sort_item);
+#ifdef UNICODE
+	int len = MultiByteToWideChar(CP_UTF8, 0, (const char *)item->key, -1, 0, 0);
+	*retPath = (const TCHAR *)apr_palloc(m_pool, len * sizeof(TCHAR));
+	MultiByteToWideChar(CP_UTF8, 0, (const char *)item->key, -1, (LPWSTR)*retPath, len);
+#else
+	if (svn_utf_cstring_from_utf8 (retPath, (const char *)item->key, m_pool) != NULL)
+		return NULL;
+#endif	
+	return (svn_wc_status_t *) item->value;
+}
+
+void SVNStatus::GetStatusString(svn_wc_status_kind status, TCHAR * string)
+{
+	TCHAR * buf;
+	switch (status)
+	{
+		case svn_wc_status_none:
+			buf = _T("none\0");
+			break;
+		case svn_wc_status_unversioned:
+			buf = _T("unversioned\0");
+			break;
+		case svn_wc_status_normal:
+			buf = _T("normal\0");
+			break;
+		case svn_wc_status_added:
+			buf = _T("added\0");
+			break;
+		case svn_wc_status_missing:
+			buf = _T("missing\0");
+			break;
+		case svn_wc_status_deleted:
+			buf = _T("deleted\0");
+			break;
+		case svn_wc_status_replaced:
+			buf = _T("replaced\0");
+			break;
+		case svn_wc_status_modified:
+			buf = _T("modified\0");
+			break;
+		case svn_wc_status_merged:
+			buf = _T("merged\0");
+			break;
+		case svn_wc_status_conflicted:
+			buf = _T("conflicted\0");
+			break;
+		case svn_wc_status_obstructed:
+			buf = _T("conflicted\0");
+			break;
+		case svn_wc_status_ignored:
+			buf = _T("ignored");
+			break;
+		case svn_wc_status_external:
+			buf = _T("external");
+			break;
+		case svn_wc_status_incomplete:
+			buf = _T("incomplete\0");
+			break;
+		default:
+			buf = _T("\0");
+			break;
+	} // switch (status) 
+	_stprintf(string, _T("%s"), buf);
+}
+
+void SVNStatus::GetStatusString(HINSTANCE hInst, svn_wc_status_kind status, TCHAR * string, int size, WORD lang)
+{
+	switch (status)
+	{
+		case svn_wc_status_none:
+			LoadStringEx(hInst, IDS_STATUSNONE, string, size, lang);
+			break;
+		case svn_wc_status_unversioned:
+			LoadStringEx(hInst, IDS_STATUSUNVERSIONED, string, size, lang);
+			break;
+		case svn_wc_status_normal:
+			LoadStringEx(hInst, IDS_STATUSNORMAL, string, size, lang);
+			break;
+		case svn_wc_status_added:
+			LoadStringEx(hInst, IDS_STATUSADDED, string, size, lang);
+			break;
+		case svn_wc_status_missing:
+			LoadStringEx(hInst, IDS_STATUSABSENT, string, size, lang);
+			break;
+		case svn_wc_status_deleted:
+			LoadStringEx(hInst, IDS_STATUSDELETED, string, size, lang);
+			break;
+		case svn_wc_status_replaced:
+			LoadStringEx(hInst, IDS_STATUSREPLACED, string, size, lang);
+			break;
+		case svn_wc_status_modified:
+			LoadStringEx(hInst, IDS_STATUSMODIFIED, string, size, lang);
+			break;
+		case svn_wc_status_merged:
+			LoadStringEx(hInst, IDS_STATUSMERGED, string, size, lang);
+			break;
+		case svn_wc_status_conflicted:
+			LoadStringEx(hInst, IDS_STATUSCONFLICTED, string, size, lang);
+			break;
+		case svn_wc_status_ignored:
+			LoadStringEx(hInst, IDS_STATUSIGNORED, string, size, lang);
+			break;
+		case svn_wc_status_obstructed:
+			LoadStringEx(hInst, IDS_STATUSOBSTRUCTED, string, size, lang);
+			break;
+		case svn_wc_status_external:
+			LoadStringEx(hInst, IDS_STATUSEXTERNAL, string, size, lang);
+			break;
+		case svn_wc_status_incomplete:
+			LoadStringEx(hInst, IDS_STATUSINCOMPLETE, string, size, lang);
+			break;
+		default:
+			LoadStringEx(hInst, IDS_STATUSNONE, string, size, lang);
+			break;
+	} // switch (status) 
+}
+
+int SVNStatus::LoadStringEx(HINSTANCE hInstance, UINT uID, LPCTSTR lpBuffer, int nBufferMax, WORD wLanguage)
+{
+	const STRINGRESOURCEIMAGE* pImage;
+	const STRINGRESOURCEIMAGE* pImageEnd;
+	ULONG nResourceSize;
+	HGLOBAL hGlobal;
+	UINT iIndex;
+#ifndef UNICODE
+	BOOL defaultCharUsed;
+#endif
+	int ret;
+
+	HRSRC hResource =  FindResourceEx(hInstance, RT_STRING, MAKEINTRESOURCE(((uID>>4)+1)), wLanguage);
+	if (!hResource)
+	{
+		//try the default language before giving up!
+		hResource = FindResource(hInstance, MAKEINTRESOURCE(((uID>>4)+1)), RT_STRING);
+		if (!hResource)
+			return 0;
+	}
+	hGlobal = LoadResource(hInstance, hResource);
+	if (!hGlobal)
+		return 0;
+	pImage = (const STRINGRESOURCEIMAGE*)::LockResource(hGlobal);
+	if(!pImage)
+		return 0;
+
+	nResourceSize = ::SizeofResource(hInstance, hResource);
+	pImageEnd = (const STRINGRESOURCEIMAGE*)(LPBYTE(pImage)+nResourceSize);
+	iIndex = uID&0x000f;
+
+	while ((iIndex > 0) && (pImage < pImageEnd))
+	{
+		pImage = (const STRINGRESOURCEIMAGE*)(LPBYTE(pImage)+(sizeof(STRINGRESOURCEIMAGE)+(pImage->nLength*sizeof(WCHAR))));
+		iIndex--;
+	}
+	if (pImage >= pImageEnd)
+		return 0;
+	if (pImage->nLength == 0)
+		return 0;
+
+#ifdef UNICODE
+	ret = pImage->nLength;
+	wcsncpy((wchar_t *)lpBuffer, pImage->achString, pImage->nLength);
+	(wchar_t)lpBuffer[ret] = 0;
+#else
+	ret = WideCharToMultiByte(CP_ACP, 0, pImage->achString, pImage->nLength, (LPSTR)lpBuffer, nBufferMax-1, ".", &defaultCharUsed);
+	(TCHAR)lpBuffer[ret] = 0;
+#endif
+	return ret;
+}
+
+void SVNStatus::getallstatus(void * baton, const char * path, svn_wc_status_t * status)
+{
+	svn_wc_status_kind * s = (svn_wc_status_kind *)baton;
+	*s = SVNStatus::GetMoreImportant(*s, status->text_status);
+	*s = SVNStatus::GetMoreImportant(*s, status->prop_status);
+}
+
+void SVNStatus::getstatushash(void * baton, const char * path, svn_wc_status_t * status)
+{
+	hashbaton_t * hash = (hashbaton_t *)baton;
+	svn_wc_status_t * statuscopy = svn_wc_dup_status (status, hash->pool);
+	apr_hash_set (hash->hash, apr_pstrdup(hash->pool, path), APR_HASH_KEY_STRING, statuscopy);
+}
+
+apr_array_header_t * SVNStatus::sort_hash (apr_hash_t *ht,
+										int (*comparison_func) (const sort_item *, const sort_item *),
+										apr_pool_t *pool)
+{
+	apr_hash_index_t *hi;
+	apr_array_header_t *ary;
+
+	/* allocate an array with only one element to begin with. */
+	ary = apr_array_make (pool, 1, sizeof(sort_item));
+
+	/* loop over hash table and push all keys into the array */
+	for (hi = apr_hash_first (pool, ht); hi; hi = apr_hash_next (hi))
+	{
+		sort_item *item = (sort_item*)apr_array_push (ary);
+
+		apr_hash_this (hi, &item->key, &item->klen, &item->value);
+	}
+
+	/* now quicksort the array.  */
+	qsort (ary->elts, ary->nelts, ary->elt_size,
+		(int (*)(const void *, const void *))comparison_func);
+
+	return ary;
+}
+
+int SVNStatus::sort_compare_items_as_paths (const sort_item *a, const sort_item *b)
+{
+	const char *astr, *bstr;
+
+	astr = (const char*)a->key;
+	bstr = (const char*)b->key;
+	return svn_path_compare_paths (astr, bstr);
+}
+
---
+++ branches/dimentiy_wix/src/TortoiseMerge/BaseView.h
@@ -0,0 +1,191 @@
+// TortoiseMerge - a Diff/Patch program
+
+// Copyright (C) 2004 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+#pragma once
+#include "DiffData.h"
+#include "LocatorBar.h"
+#include "LineDiffBar.h"
+
+class CMainFrame;
+
+/**
+ * \ingroup TortoiseMerge
+ *
+ * View class providing the basic functionality for
+ * showing diffs. Has three parent classes which inherit
+ * from this base class: CLeftView, CRightView and CBottomView.
+ */
+class CBaseView : public CView
+{
+friend class CLineDiffBar;
+public:
+	CBaseView();
+	virtual ~CBaseView();
+
+public:
+	/**
+	 * Indicates that the underlying document has been updated. Reloads all
+	 * data and redraws the view.
+	 */
+	virtual void	DocumentUpdated();
+	/**
+	 * Returns the number of lines visible on the view.
+	 */
+	int				GetScreenLines();
+	/**
+	 * Scrolls the view to the given line.
+	 * \param nNewTopLine The new top line to scroll the view to
+	 * \param bTrackScrollBar If TRUE, then the scrollbars are affected too.
+	 */
+	void			ScrollToLine(int nNewTopLine, BOOL bTrackScrollBar = TRUE);
+	void			ScrollAllToLine(int nNewTopLine, BOOL bTrackScrollBar = TRUE);
+	void			GoToLine(int nNewLine, BOOL bAll = TRUE);
+
+	void			SelectLines(int nLine1, int nLine2 = -1);
+	inline BOOL		IsHidden() {return m_bIsHidden;}
+	inline void		SetHidden(BOOL bHidden) {m_bIsHidden = bHidden;}
+	inline BOOL		IsModified() {return m_bModified;}
+	void			SetModified(BOOL bModified = TRUE) {m_bModified = bModified;}
+
+	CStringArray *	m_arDiffLines;		///< Array of Strings containing all lines of the text file
+	CDWordArray	*	m_arLineStates;		///< Array of Strings containing a diff state for each text line
+	CString			m_sWindowName;		///< The name of the view which is shown as a window title to the user
+	CString			m_sFullFilePath;	///< The full path of the file shown
+
+	BOOL			m_bViewWhitespace;	///< If TRUE, then SPACE and TAB are shown as special characters
+	int				m_nTopLine;			///< The topmost text line in the view
+
+	static CLocatorBar * m_pwndLocator;	///< Pointer to the locator bar on the left
+	static CLineDiffBar * m_pwndLineDiffBar;	///< Pointer to the line diff bar at the bottom
+	static CStatusBar * m_pwndStatusBar;///< Pointer to the status bar
+	static CMainFrame * m_pMainFrame;	///< Pointer to the mainframe
+
+	void			GoToFirstDifference();
+protected:
+	virtual BOOL	PreCreateWindow(CREATESTRUCT& cs);
+	virtual void	OnDraw(CDC * pDC);
+	virtual INT_PTR OnToolHitTest(CPoint point, TOOLINFO* pTI) const;
+	virtual BOOL	PreTranslateMessage(MSG* pMsg);
+	BOOL			OnToolTipNotify(UINT id, NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void	OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void	OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg BOOL	OnEraseBkgnd(CDC* pDC);
+	afx_msg int		OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void	OnDestroy();
+	afx_msg void	OnSize(UINT nType, int cx, int cy);
+	afx_msg BOOL	OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+	afx_msg BOOL	OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
+	afx_msg void	OnKillFocus(CWnd* pNewWnd);
+	afx_msg void	OnSetFocus(CWnd* pOldWnd);
+	afx_msg void	OnContextMenu(CWnd* pWnd, CPoint point);
+	afx_msg void	OnMergeNextdifference();
+	afx_msg void	OnMergePreviousdifference();
+
+	DECLARE_MESSAGE_MAP()
+
+protected:
+	void			DrawMargin(CDC *pdc, const CRect &rect, int nLineIndex);
+	void			DrawSingleLine(CDC *pdc, const CRect &rc, int nLineIndex);
+	void			ExpandChars(LPCTSTR pszChars, int nOffset, int nCount, CString &line);
+
+	BOOL			IsLineRemoved(int nLineIndex);
+
+	void			RecalcVertScrollBar(BOOL bPositionOnly = FALSE);
+	void			RecalcHorzScrollBar(BOOL bPositionOnly = FALSE);
+
+	void			ScrollToChar(int nNewOffsetChar, BOOL bTrackScrollBar = TRUE);
+
+	void			OnDoMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+	void			OnDoHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar, CBaseView * master);
+	void			OnDoVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar, CBaseView * master);
+
+	int				GetTabSize() {return m_nTabSize;}
+
+	int				GetLineActualLength(int index);
+	int				GetLineCount();
+	void			CalcLineCharDim();
+	int				GetLineHeight();
+	int				GetCharWidth();
+	int				GetMaxLineLength();
+	int				GetLineLength(int index);
+	int				GetScreenChars();
+	int				GetAllMinScreenChars();
+	int				GetAllMaxLineLength();
+	int				GetAllLineCount();
+	int				GetAllMinScreenLines();
+	LPCTSTR			GetLineChars(int index);
+	CFont *			GetFont(BOOL bItalic = FALSE, BOOL bBold = FALSE, BOOL bStrikeOut = FALSE);
+	int				GetLineFromPoint(CPoint point);
+	int				GetMarginWidth();
+
+	virtual BOOL	ShallShowContextMenu(CDiffData::DiffStates state, int nLine);
+	virtual	void	OnContextMenu(CPoint point, int nLine);
+	/**
+	 * Updates the status bar pane. Call this if the document changed.
+	 */
+	void			UpdateStatusBar();
+protected:
+	UINT			m_nStatusBarID;		///< The ID of the status bar pane used by this view. Must be set by the parent class.
+
+	BOOL			m_bModified;
+	BOOL			m_bFocused;
+	BOOL			m_bViewLinenumbers;
+	BOOL			m_bIsHidden;
+	int				m_nLineHeight;
+	int				m_nCharWidth;
+	int				m_nMaxLineLength;
+	int				m_nScreenLines;
+	int				m_nScreenChars;
+	int				m_nOffsetChar;
+	int				m_nTabSize;
+	int				m_nDigits;
+
+	int				m_nSelBlockStart;
+	int				m_nSelBlockEnd;
+
+	int				m_nMouseLine;
+
+	HICON			m_hAddedIcon;
+	HICON			m_hRemovedIcon;
+	HICON			m_hConflictedIcon;
+
+	LOGFONT			m_lfBaseFont;
+	CFont *			m_apFonts[8];
+
+	CBitmap *		m_pCacheBitmap;
+	CDC *			m_pDC;
+	CToolTipCtrl	m_ToolTips;
+	
+	char			m_szTip[MAX_PATH+1];
+	wchar_t			m_wszTip[MAX_PATH+1];
+	// These three pointers lead to the three parent
+	// classes CLeftView, CRightView and CBottomView
+	// and are used for the communication between
+	// the views (e.g. synchronized scrolling, ...)
+	// To find out which parent class this object
+	// is made of just compare e.g. (m_pwndLeft==this).
+	static CBaseView * m_pwndLeft;		///< Pointer to the left view. Must be set by the CLeftView parent class.
+	static CBaseView * m_pwndRight;		///< Pointer to the right view. Must be set by the CRightView parent class.
+	static CBaseView * m_pwndBottom;	///< Pointer to the bottom view. Must be set by the CBottomView parent class.
+public:
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnEditCopy();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
+
