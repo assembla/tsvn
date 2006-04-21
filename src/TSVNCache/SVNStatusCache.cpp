1c62d83720ab22cb9f52d9caab0b07f50edadad3
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-04-21 16:48:10 +0000

    Fix broken build and merge revision 6348 from trunk: * Increase the version number of the saved cached data after the fix yesterday.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.3.x@6350 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TSVNCache/SVNStatusCache.cpp
+++ src/TSVNCache/SVNStatusCache.cpp
@@ -58,7 +58,7 @@ void CSVNStatusCache::Create()
 		if (pFile)
 		{
 			LOADVALUEFROMFILE(value);
-			if (value != 1)
+			if (value != 2)
 			{
 				goto error;
 			}
@@ -132,7 +132,7 @@ bool CSVNStatusCache::SaveCache()
 		_tfopen_s(&pFile, path, _T("wb"));
 		if (pFile)
 		{
-			value = 1;		// 'version'
+			value = 2;		// 'version'
 			WRITEVALUETOFILE(value);
 			value = (int)m_pInstance->m_directoryCache.size();
 			WRITEVALUETOFILE(value);
@@ -350,7 +350,7 @@ CStatusCacheEntry CSVNStatusCache::GetStatusForPath(const CTSVNPath& path, DWORD
 		CCachedDirectory * cachedDir = GetDirectoryCacheEntry(path.GetContainingDirectory());
 		if (cachedDir != NULL)
 		{
-			m_mostRecentStatus = cachedDir->GetStatusForMember(path, bRecursive, bFetch);
+			m_mostRecentStatus = cachedDir->GetStatusForMember(path, bRecursive);
 			return m_mostRecentStatus;
 		}
 	}
