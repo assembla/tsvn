ddb07025319457bb844020fa1d7ed6b1c144128c
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2006-09-24 18:18:18 +0000

    * add description of the RLE compression used * flush streamed vector<>s a.s.a.p. to minimize memory usage * set EOL style to native
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/LogCaching@7593 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/PackedDWORDOutStream.h
+++ src/LogCache/PackedDWORDOutStream.h
@@ -25,15 +25,40 @@
 //		  (i.e the first byte contains bits 0..6, the next
 //		   byte 7 .. 13 and so on)
 //
+//		However, the incomming data gets run-length encoded
+//		before being written to the stream. This is because 
+//		most integer streams have larger sections of constant
+//		values (saves approx. 10% of total log cache size).
+//
+//		Encoding scheme:
+//
+//		* 0 indicates a packed value. It is followed by the
+//		  the number of repititions followed by the value itself
+//		* an compressed value is stored as value+1
+//		* initial lastValue is 0
+//
 ///////////////////////////////////////////////////////////////
 
 class CPackedDWORDOutStreamBase : public CBinaryOutStreamBase
 {
 protected:
 
+	// differential storage:
+	// the last value we received through Add() and the
+	// how many times we already got that value in a row
+
+	DWORD lastValue;
+	DWORD count;
+
 	// add data to the stream
 
+	void InternalAdd (DWORD value);
+	void FlushLastValue();
+
+	// compression (RLE) support
+
 	void Add (DWORD value);
+	void WriteThisStream (CCacheFileOutBuffer* buffer);
 
 public:
 
@@ -43,6 +68,32 @@ public:
 						      , SUB_STREAM_ID anID);
 	virtual ~CPackedDWORDOutStreamBase() {};
 };
+
+///////////////////////////////////////////////////////////////
+// compress incomming data and write it to the stream
+///////////////////////////////////////////////////////////////
+
+inline void CPackedDWORDOutStreamBase::Add (DWORD value)
+{
+	if (value == lastValue)
+	{
+		++count;
+	}
+	else
+	{
+		if (count == 1)
+		{
+			InternalAdd (lastValue+1);
+		}
+		else
+		{
+			FlushLastValue();
+			count = 1;
+		}
+
+		lastValue = value;
+	}
+}
 
 ///////////////////////////////////////////////////////////////
 //
@@ -89,9 +140,16 @@ S& operator<< (S& stream, const std::vector<V>& data)
 {
 	typedef typename std::vector<V>::const_iterator IT;
 
+	// write total entry count and entries
+
 	stream.Add ((typename S::value_type)data.size());
 	for (IT iter = data.begin(), end = data.end(); iter != end; ++iter)
 		stream.Add ((typename S::value_type)(*iter));
+
+	// just close the stream 
+	// (i.e. flush to disk and empty internal buffers)
+
+	stream.AutoClose();
 
 	return stream;
 }
