39d3423ea5f46e84fdcb2ea100f9dbbdde0ef8e8
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2007-05-13 16:34:01 +0000

    * factor out LogCacheLib project   (to be linked with TSVNProc)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/LogCaching@9401 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/stdafx.cpp
+++ src/LogCache/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// LogCache.pch will be the pre-compiled header
+// LogCacheLib.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
