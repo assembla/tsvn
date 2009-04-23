f1c7484622078430875c3ddaac7d4d7fb16af7b5
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-04-23 18:04:12 +0000

    Merge r16244 from trunk: * Use GET_X_LPARAM() and GET_Y_LPARAM() instead of custom casts. * check for -1 instead of < 0 to avoid problems with multi-monitor setups.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.6.x@16245 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseBlame/TortoiseBlame.cpp
+++ src/TortoiseBlame/TortoiseBlame.cpp
@@ -1730,7 +1730,7 @@ LRESULT CALLBACK WndBlameProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 			mevt.dwHoverTime = HOVER_DEFAULT;
 			mevt.hwndTrack = app.wBlame;
 			::TrackMouseEvent(&mevt);
-			POINT pt = {((int)(short)LOWORD(lParam)), ((int)(short)HIWORD(lParam))};
+			POINT pt = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
 			ClientToScreen(app.wBlame, &pt);
 			pt.x += 15;
 			pt.y += 15;
@@ -1743,7 +1743,7 @@ LRESULT CALLBACK WndBlameProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 				ti.uId = 0;
 				SendMessage(app.hwndTT, TTM_TRACKACTIVATE, TRUE, (LPARAM)&ti);
 			}
-			int y = ((int)(short)HIWORD(lParam));
+			int y = GET_Y_LPARAM(lParam);
 			LONG_PTR line = app.SendEditor(SCI_GETFIRSTVISIBLELINE);
 			LONG_PTR height = app.SendEditor(SCI_TEXTHEIGHT);
 			line = line + (y/height);
@@ -1767,7 +1767,7 @@ LRESULT CALLBACK WndBlameProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 		// fall through
 	case WM_LBUTTONDOWN:
 		{
-			int y = ((int)(short)HIWORD(lParam));
+			int y = GET_Y_LPARAM(lParam);
 			LONG_PTR line = app.SendEditor(SCI_GETFIRSTVISIBLELINE);
 			LONG_PTR height = app.SendEditor(SCI_TEXTHEIGHT);
 			line = line + (y/height);
@@ -1806,12 +1806,12 @@ LRESULT CALLBACK WndBlameProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPa
 				break;;
 			int xPos = GET_X_LPARAM(lParam);
 			int yPos = GET_Y_LPARAM(lParam);
-			if ((xPos < 0)||(yPos < 0))
+			if ((xPos == -1)||(yPos == -1))
 			{
 				// requested from keyboard, not mouse pointer
 				// use the center of the window
 				RECT rect;
-				GetClientRect(app.wBlame, &rect);
+				GetWindowRect(app.wBlame, &rect);
 				xPos = rect.right-rect.left;
 				yPos = rect.bottom-rect.top;
 			}
