32f92d634d940d9e81a2d3806534d9dc1af6091b
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2011-10-26 06:30:16 +0000

    Add dos2unix for consistent line endings and some documentation for better understanding to mergefromtrunk.bat
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.7.x@22193 154be670-7969-47af-9ab0-c6fb378d2295

--- Languages/mergefromtrunk.bat
+++ Languages/mergefromtrunk.bat
@@ -1,23 +1,30 @@
 @echo off
+
+rem Merge translations from trunk and branch into the branch translation
+rem Needs the following tools installed in your search path:
+rem - Gnu gettext to combine and merge the .po files
+rem - dos2unix to make sure that the line endings are consistent
+
 setlocal
-SET TRUNK=..\..\..\trunk\Languages
+set TRUNK=..\..\..\trunk\Languages
 
-REM Merge translations from trunk and branch into the branch translation
-REM msgcat deliberately uses trunk first to overwrite old translations 
-REM with newer ones from trunk!
-REM If we used branch first, we'd get only missing translations from trunk
+rem msgcat deliberately uses trunk first to overwrite old translations 
+rem with newer ones from trunk!
+rem If we used branch first, we'd get only missing translations from trunk
 
-FOR /D %%L IN (*.) DO (
+for /D %%L in (*.) DO (
   echo Merging %%L
-  IF EXIST %%L\TortoiseUI.po (
+  if exist %%L\TortoiseUI.po (
     echo - GUI Translation
     msgcat --use-first %TRUNK%\%%L\TortoiseUI.po %%L\TortoiseUI.po -o temp.po 2>nul
     msgmerge temp.po TortoiseUI.pot -o %%L\TortoiseUI.po 2>nul
+	dos2unix %%L\TortoiseUI.po
   )
-  IF EXIST %%L\TortoiseDoc.po (
+  if exist %%L\TortoiseDoc.po (
     echo - Doc Translation
     msgcat --use-first %TRUNK%\%%L\TortoiseDoc.po %%L\TortoiseDoc.po -o temp.po
     msgmerge temp.po TortoiseDoc.pot -o %%L\TortoiseDoc.po
+	dos2unix %%L\TortoiseDoc.po
   )
 )
 
