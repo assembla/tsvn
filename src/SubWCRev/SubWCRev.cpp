72f70d07d03de9dca5f66529c8d1435a13513f1d
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-12-21 19:41:12 +0000

    Merged r22407, r22408 and r22409 from trunk: * Unescape the url before adding it to the url combo box. (Fixes issue #170) : svn url fomatting broken on 'switch' * Call the main refresh function on a right-click refresh. (Fixes issue #171) : Right-Click Refresh not working in Repo Browser correctly * Update issue #169. avoid memory allocation in case the API returns an error.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.7.x@22410 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SubWCRev/SubWCRev.cpp
+++ src/SubWCRev/SubWCRev.cpp
@@ -484,9 +484,12 @@ int _tmain(int argc, _TCHAR* argv[])
     // convert the path to its short form, then back to its long form.
     // That will fix the wrong casing of the path.
     int shortlen = GetShortPathName(fullPath, NULL, 0);
-    std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
-    GetShortPathName(fullPath, shortPath.get(), shortlen+1);
-    GetLongPathName(shortPath.get(), fullPath, reqLen);
+    if (shortlen)
+    {
+        std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
+        if (GetShortPathName(fullPath, shortPath.get(), shortlen+1))
+            GetLongPathName(shortPath.get(), fullPath, reqLen);
+    }
     wc = fullPath;
     if (dst)
     {
@@ -494,9 +497,12 @@ int _tmain(int argc, _TCHAR* argv[])
         fullPath = new TCHAR[reqLen+1];
         GetFullPathName(dst, reqLen, fullPath, NULL);
         shortlen = GetShortPathName(fullPath, NULL, 0);
-        std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
-        GetShortPathName(fullPath, shortPath.get(), shortlen+1);
-        GetLongPathName(shortPath.get(), fullPath, reqLen);
+        if (shortlen)
+        {
+            std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
+            if (GetShortPathName(fullPath, shortPath.get(), shortlen+1))
+                GetLongPathName(shortPath.get(), fullPath, reqLen);
+        }
         dst = fullPath;
     }
     if (src)
@@ -505,9 +511,12 @@ int _tmain(int argc, _TCHAR* argv[])
         fullPath = new TCHAR[reqLen+1];
         GetFullPathName(src, reqLen, fullPath, NULL);
         shortlen = GetShortPathName(fullPath, NULL, 0);
-        std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
-        GetShortPathName(fullPath, shortPath.get(), shortlen+1);
-        GetLongPathName(shortPath.get(), fullPath, reqLen);
+        if (shortlen)
+        {
+            std::unique_ptr<TCHAR[]> shortPath(new TCHAR[shortlen+1]);
+            if (GetShortPathName(fullPath, shortPath.get(), shortlen+1))
+                GetLongPathName(shortPath.get(), fullPath, reqLen);
+        }
         src = fullPath;
     }
 
