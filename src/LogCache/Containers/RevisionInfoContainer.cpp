7a430989e56703a3a603c18c6f0b126eee6a8238
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-04-26 16:56:11 +0000

    Merged r22834, r22835 and r22837 from trunk: * Empty logs may be caused by NULL log cache objects, so shortcut the log processing in that case. (Fixes issue #281) : Log view may crash for empty logs. * When we update ("splice") the log cache for revisions and receive additional changes for an already cached revision, we will corrupt the log cache. Fix: read the old data offset of the next entry before writing the new data offset. (Fixes issue #282) : Vector index exception when updating the log cache. * Update changelog
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.7.x@22838 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/Containers/RevisionInfoContainer.cpp
+++ src/LogCache/Containers/RevisionInfoContainer.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2010 - TortoiseSVN
+// Copyright (C) 2007-2010, 2012 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -130,6 +130,9 @@ void CRevisionInfoContainer::UpdateChanges
 
     // splice
 
+    index_t firstChange = 0;
+    index_t firstCopy = 0;
+
     index_mapping_t::const_iterator mapEnd = indexMap.end();
     for (index_t i = 0, count = size(); i < count; ++i)
     {
@@ -169,10 +172,7 @@ void CRevisionInfoContainer::UpdateChanges
         {
             // keep existing data
 
-            index_t firstChange = changesOffsets[i];
             index_t lastChange = changesOffsets[i+1];
-
-            index_t firstCopy = copyFromOffsets[i];
             index_t lastCopy = copyFromOffsets[i+1];
 
             // standard per-path info
@@ -209,6 +209,9 @@ void CRevisionInfoContainer::UpdateChanges
         }
 
         // update positions
+
+        firstChange = changesOffsets[i+1];
+        firstCopy = copyFromOffsets[i+1];
 
         changesOffsets[i+1] = static_cast<index_t> (changes.size());
         copyFromOffsets[i+1] = static_cast<index_t> (copyFromPaths.size());
