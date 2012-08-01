cb222c851ec39f1c9f1a924d5a95620b1c6e1c68
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2012-08-01 09:45:57 +0000

    Merged revision(s) 23114, 23116, 23118-23121 from trunk: First check if there's a lock owner before trying to access it. (Fixes issue #377) : file lock without owner crashes status cache
    ........
    When w/c state is to be shown in the rev graph, we will read
    the w/c info and store a number of (rev, path) pairs. If the
    respective path is not versioned, we cannot use logcache-based
    representations for it.
    
    Because those nodes will never be visible in the graph (due
    to the missing logcache info), it is safe to simply ignore
    those pairs.
    
    (Fixes issue #379) : Access violation when reading the w/c state for the revision graph
    ........
    Don't crash when the author ID is NO_INDEX;
    return an empty author instead.
    Also, use author ID 0 for empty authors.
    
    (Fixes issue #380) : Crash in log reporter when author information is empty
    ........
    Follow-up to r22835: Fix the source offset handling for revprops
    and merge info as well.
    
    (Fixes issue #381) : Vector index exception when updating the revprops in log cache
    ........
    End log iteration when the path info is not available
    (i.e. invalid).
    
    (Fixes issue #382) : Don't crash in log cache when path info is not available
    
    ........
    Bring change log up-to-date.
    ........
    
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.7.x@23122 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/Containers/RevisionInfoContainer.cpp
+++ src/LogCache/Containers/RevisionInfoContainer.cpp
@@ -248,6 +248,7 @@ void CRevisionInfoContainer::UpdateMergers
 
     // splice
 
+    index_t firstMerge = 0;
     index_mapping_t::const_iterator mapEnd = indexMap.end();
     for (index_t i = 0, count = size(); i < count; ++i)
     {
@@ -275,7 +276,6 @@ void CRevisionInfoContainer::UpdateMergers
         {
             // keep existing data
 
-            index_t firstMerge = mergedRevisionsOffsets[i];
             index_t lastMerge = mergedRevisionsOffsets[i+1];
 
             mergedFromPaths.insert (mergedFromPaths.end()
@@ -294,6 +294,7 @@ void CRevisionInfoContainer::UpdateMergers
 
         // update positions
 
+        firstMerge = mergedRevisionsOffsets[i+1];
         mergedRevisionsOffsets[i+1] = static_cast<index_t> (mergedFromPaths.size());
     }
 }
@@ -328,6 +329,7 @@ void CRevisionInfoContainer::UpdateUserRevProps
 
     // splice
 
+    index_t firstProp = 0;
     index_t valueIndex = 0;
 
     index_mapping_t::const_iterator mapEnd = indexMap.end();
@@ -357,7 +359,6 @@ void CRevisionInfoContainer::UpdateUserRevProps
         {
             // keep existing data
 
-            index_t firstProp = userRevPropOffsets[i];
             index_t lastProp = userRevPropOffsets[i+1];
 
             userRevPropNames.insert (userRevPropNames.end()
@@ -370,6 +371,7 @@ void CRevisionInfoContainer::UpdateUserRevProps
 
         // update positions
 
+        firstProp = userRevPropOffsets[i+1];
         userRevPropOffsets[i+1] = static_cast<index_t> (userRevPropNames.size());
     }
 
@@ -425,7 +427,7 @@ void CRevisionInfoContainer::AppendNewEntries
     {
         // append a new revision info (no info available)
 
-        authors.insert (authors.end(), toAppend, (index_t) NO_INDEX);
+        authors.insert (authors.end(), toAppend, 0);
         timeStamps.insert (timeStamps.end(), toAppend, 0);
         presenceFlags.insert (presenceFlags.end(), toAppend, 0);
         rootPaths.insert (rootPaths.end(), toAppend, (index_t) NO_INDEX);
