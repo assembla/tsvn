23dc55308eab47721907d93721bc63cb7b2f80eb
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-02-01 18:11:04 +0000

    Merged revision(s) r23876 from trunk: * Validate the drawing area after the custom painting of the control. This shouldn't be necessary, but it appears some plugins can mess up the painting somehow. See this thread for more details: http://tortoisesvn.tigris.org/ds/viewMessage.do?dsForumId=4061&dsMessageId=3011957
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.7.x@23877 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatusListCtrl.cpp
+++ src/SVN/SVNStatusListCtrl.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2012 - TortoiseSVN
+// Copyright (C) 2003-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -4681,6 +4681,7 @@ void CSVNStatusListCtrl::OnPaint()
         }
         ReleaseDC(pDC);
     }
+    ValidateRect(NULL);
 }
 
 // prevent users from extending our hidden (size 0) columns
