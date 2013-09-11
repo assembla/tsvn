3973e3f06498139550bcd887bb7e752ae1925ca7
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-09-11 20:02:20 +0000

    Merged revision(s) r24755, r24756 from trunk: * Fix ext build files due to bad character and thus really disable /Oy after r24729. * Patch from Sergey Azarkevich: set svn:ignores instead of svn:global-ignores when requested. (Fixes issue #557) : Ignored items always set global ignores
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.8.x@24758 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatusListCtrl.cpp
+++ src/SVN/SVNStatusListCtrl.cpp
@@ -3849,7 +3849,7 @@ void CSVNStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
                 OnIgnoreMask(filepath, true);
                 break;
             case IDSVNLC_IGNORE:
-                OnIgnore(filepath, true);
+                OnIgnore(filepath, false);
                 break;
             case IDSVNLC_IGNOREGLOBAL:
                 OnIgnore(filepath, true);
