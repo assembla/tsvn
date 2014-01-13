3c1f48c495082f4bc2571277a024196fc421535b
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2014-01-13 10:30:51 +0000

    Greatly improve mergefromtrunk.bat, so that it reverts 99% of the merges that didn't add any new translations from trunk
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.8.x@25137 154be670-7969-47af-9ab0-c6fb378d2295

--- Languages/mergefromtrunk.bat
+++ Languages/mergefromtrunk.bat
@@ -1,34 +1,47 @@
 @echo off
-
+cls
 rem Merge translations from trunk and branch into the branch translation
 rem Needs the following tools installed in your search path:
 rem - Gnu gettext to combine and merge the .po files
 rem - dos2unix to make sure that the line endings are consistent
 
-setlocal
+setlocal EnableDelayedExpansion
 set TRUNK=..\..\..\trunk\Languages
-
-rem msgcat deliberately uses trunk first to overwrite old translations 
-rem with newer ones from trunk!
-rem If we used branch first, we'd get only missing translations from trunk
 
 for /D %%L in (*.) DO (
   echo Merging %%L
   if exist %%L\TortoiseUI.po (
     echo - GUI Translation
-    msgcat --use-first %TRUNK%\%%L\TortoiseUI.po %%L\TortoiseUI.po -o temp.po 2>nul
-    msgmerge temp.po TortoiseUI.pot -o %%L\TortoiseUI.po 2>nul
-	dos2unix %%L\TortoiseUI.po
+    call :mergeit %%L\TortoiseUI.po TortoiseUI.pot
   )
   if exist %%L\TortoiseDoc.po (
     echo - Doc Translation
-    msgcat --use-first %TRUNK%\%%L\TortoiseDoc.po %%L\TortoiseDoc.po -o temp.po
-    msgmerge temp.po TortoiseDoc.pot -o %%L\TortoiseDoc.po
-	dos2unix %%L\TortoiseDoc.po
+    call :mergeit %%L\TortoiseDoc.po TortoiseDoc.pot
   )
+  echo.
 )
+goto ende
 
-del temp.po
-del messages.mo
+:mergeit
+    rem "msgcat" deliberately uses trunk first to overwrite old translations 
+    rem with newer ones from trunk!
+    rem If we used branch first, we'd get only missing translations from trunk.
+    msgcat --use-first %TRUNK%\%1 %1 -o temp.po 2>nul
+    msgmerge temp.po %2 -o %1 2>nul
+    dos2unix %1 2>nul
+    rem Count the changes. If there's only one changed line, it's the "PO-Revision-Date" line
+    rem which means that there is no real change at all.
+    rem In this case revert the file.
+    for /F %%I in ('svn diff "%1" ^| find /c "@@"') do set chg=%%I
+    if %chg% EQU 1 (
+    rem "svn revert" fails for paths which contain an "@" sign, like sr@latin.
+    rem So we just append an "@" to the end of the path, because svn only 
+    rem cares about the last "@".
+    svn revert "%1@"
+    )
+    exit /b
 
+:ende
+del temp.po 2>nul
+del messages.mo 2>nul
 endlocal
