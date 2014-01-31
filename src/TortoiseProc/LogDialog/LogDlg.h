170c52bb7f1fa9600c5b5d398cfb4beb3c9b3fee
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2014-01-31 21:04:27 +0000

    Merged revision(s) r25180 from trunk: * When setting a range of revisions to select on log dialog start, ensure that all the specified revisions are fetched and shown on the first fetch. (Fixes issue #611) : log dialog does not fetch all revisions specified in merge dialog
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.8.x@25181 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/LogDialog/LogDlg.h
+++ src/TortoiseProc/LogDialog/LogDlg.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2013 - TortoiseSVN
+// Copyright (C) 2003-2014 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -429,6 +429,7 @@ private:
     HACCEL              m_hAccel;
 
     CStoreSelection*    m_pStoreSelection;
+    bool                m_bEnsureSelection;
     CLogDataVector      m_logEntries;
     size_t              m_prevLogEntriesSize;
 
