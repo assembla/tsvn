a63ee25a2467347043fbe5686c0d0cbb1c71e37d
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2014-03-03 20:49:40 +0000

    Merged revision(s) r25263 from trunk: * fix resource leak: Close the event handle in the destructor.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/branches/1.8.x@25266 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNFolderStatus.cpp
+++ src/SVN/SVNFolderStatus.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2013 - TortoiseSVN
+// Copyright (C) 2003-2014 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -107,6 +107,7 @@ SVNFolderStatus::SVNFolderStatus(void)
 
 SVNFolderStatus::~SVNFolderStatus(void)
 {
+    CloseHandle(m_hInvalidationEvent);
     svn_pool_destroy(rootpool);
 }
 
